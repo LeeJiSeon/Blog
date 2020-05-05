373364b8440d40e941a82cfebd0125804776b619
LeeJiSun <j5119s@naver.com>
2020-05-05 14:43:12 +0900

    DB구축(2)

---
+++ src/main/java/com/blogproject/web/board/dao/BoardDAOImpl.java
@@ -0,0 +1,48 @@
+package com.blogproject.web.board.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.blogproject.web.board.model.BoardVO;
+
+@Repository
+public class BoardDAOImpl implements BoardDAO {
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	@Override
+	public List<BoardVO> getBoardList() throws Exception {
+		return sqlSession.selectList("com.blogproject.web.board.boardMapper.getBoardList");
+	}
+	
+	@Override
+	public BoardVO getBoardContent(int bid) throws Exception {
+		return sqlSession.selectOne("com.blogproject.web.board.boardMapper.getBoardContent", bid);
+	}
+	
+	@Override
+	public int insertBoard(BoardVO boardVO) throws Exception {
+		return sqlSession.insert("com.blogproject.web.board.boardMapper.inserBoard", boardVO);
+	}
+	
+	@Override
+	public int updateBoard(BoardVO boardVO) throws Exception {
+		return sqlSession.update("com.blogproject.web.board.boardMapper.updateBoard", boardVO);
+	}
+	
+	@Override
+	public int deleteBoard(int bid) throws Exception {
+		return sqlSession.update("com.blogproject.web.board.boardMapper.deleteBoard", bid);
+	}
+	
+	@Override
+	public int updateViewCnt(int bid) throws Exception {
+		return sqlSession.update("com.blogproject.web.board.boardMapper.updateViewCnt", bid);
+	}
+	
+}
